ad83e9be1e80c689ef5b70c553aca0827ffe0e09
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-05-06 10:48:20 +0530

    jenkins

--- vars/pipelineGlobals.groovy
+++ vars/pipelineGlobals.groovy
@@ -1,15 +1,15 @@
 def getAccountID(String environment){
     switch(environment) { 
         case 'dev': 
-            return "315069654700"
+            return "506369992735"
         case 'qa':
-            return "315069654700"
+            return "506369992735"
         case 'uat':
-            return "315069654700"
+            return "506369992735"
         case 'pre-prod':
-            return "315069654700"
+            return "506369992735"
         case 'prod':
-            return "315069654700"
+            return "506369992735"
         default:
             return "nothing"
     } 
